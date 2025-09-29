765a220fd8db0722456c4636fc0df5b45ffac2eb
Viktor Arkhipov <palolenin@gmail.com>
2025-09-29 22:35:02 +0300

    Сообщение

--- src/ScaffoldingCalculator.jsx
+++ src/ScaffoldingCalculator.jsx
@@ -1,4 +1,4 @@
-/import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { saveAs } from 'file-saver';
 import { Packer, Document, Paragraph, TextRun } from 'docx';
 
